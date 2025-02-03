480f3f170a8ef5b9087051fcb70f902ecd3b8b08
Mfikryyyyyy11 <mfikryhaikal8@gmail.com>
2025-02-03 20:41:40 +0700

    add increment and decrement cart item update cart

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -158,10 +158,73 @@ async function updateCartItem(req, res) {
   }
 }
 
+async function incrementCartItem(req, res) {
+  try {
+    const userId = req.user.id;
+    const { cartItemId } = req.body;
+
+    const cartItem = await prisma.cartItem.findUnique({
+      where: {
+        id: cartItemId,
+      },
+      include: {
+        cart: true,
+      },
+    });
+
+    if (!cartItem || cartItem.cart.userId !== userId) {
+      return res.status(404).json({ message: 'Item not found in cart' });
+    }
+
+    const updatedItem = await prisma.cartItem.update({
+      where: { id: cartItemId },
+      data: { quantity: cartItem.quantity + 1 },
+    });
+
+    return res.status(200).json(updatedItem);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+}
+
+async function decrementCartItem(req, res) {
+  try {
+    const userId = req.user.id;
+    const { cartItemId } = req.body;
+
+    const cartItem = await prisma.cartItem.findUnique({
+      where: { id: cartItemId },
+      include: { cart: true },
+    });
+
+    if (!cartItem || cartItem.cart.userId !== userId) {
+      return res.status(404).json({ message: 'Item not found in cart' });
+    }
+
+    if (cartItem.quantity === 1) {
+      await prisma.cartItem.delete({ where: { id: cartItemId } });
+      return res.status(200).json({ message: 'Item removed from cart' });
+    }
+
+    const updatedItem = await prisma.cartItem.update({
+      where: { id: cartItemId },
+      data: { quantity: cartItem.quantity - 1 },
+    });
+
+    res.status(200).json(updatedItem);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+}
+
 module.exports = {
   getAllProductsInCart,
   addProductToCart,
   removeProductFromCart,
   clearProductFromCart,
   updateCartItem,
+  incrementCartItem,
+  decrementCartItem,
 };
--- src/routes/cartRoutes.js
+++ src/routes/cartRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { isAuthenticated } = require('../middlewares/jwtMiddleware');
-const { getAllProductsInCart, addProductToCart, removeProductFromCart, clearProductFromCart, updateCartItem } = require('../controllers/cartController');
+const { getAllProductsInCart, addProductToCart, removeProductFromCart, clearProductFromCart, updateCartItem, incrementCartItem, decrementCartItem } = require('../controllers/cartController');
 
 const router = express.Router();
 
@@ -9,5 +9,7 @@ router.post('/add', isAuthenticated, addProductToCart);
 router.delete('/:id', isAuthenticated, removeProductFromCart);
 router.delete('/clear', isAuthenticated, clearProductFromCart);
 router.put('/', isAuthenticated, updateCartItem);
+router.put('/increment', isAuthenticated, incrementCartItem);
+router.put('/decrement', isAuthenticated, decrementCartItem);
 
 module.exports = router;
